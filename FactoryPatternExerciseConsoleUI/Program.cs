50ab69e633a27e74f17276a0003b21091649a506
CHJohnston <cdh.johnston@gmail.com>
2020-07-08 09:32:42 -0500

    Updated with Input Validation

--- FactoryPatternExerciseConsoleUI/Program.cs
+++ FactoryPatternExerciseConsoleUI/Program.cs
@@ -9,19 +9,27 @@ namespace FactoryPatternExerciseConsoleUI
          //Build a Vechile based on the number of tires entered by the user
                     
         Console.WriteLine("Let's Build a New Vechicle!");
-        Console.Write("Enter the number of Tires for your Vechicle (2, 4, or 6): ");       
-          
-        string numberofTires = Console.ReadLine();
-        
-        if (numberofTires == "2" || numberofTires == "4" || numberofTires == "6")                    
-        {
-            IVehicle vehicle = VechicleFactory.GetVehicle(numberofTires);
-            vehicle.Drive();           
-        } 
-        else
-        {
-            Console.WriteLine($"{numberofTires} is not a valid input!!");
-        }           
+
+        //Check for valid values, if not keep asking user until the valid number is entered
+        //If the input value is valid, then create the vehicle using the Factory Pattern Vechicle Factory
+        Console.Write("Enter the number of Tires for your Vechicle (2, 4, or 6): ");
+        string numberofTires = Console.ReadLine();       
+        bool validInput = false;
+        while (validInput == false)
+            {
+                if (numberofTires == "2" || numberofTires == "4" || numberofTires == "6")
+                {
+                    //This is a valid input -- create the vehicle using the Factory Pattern IVechicle Factory
+                    validInput = true;                    
+                    IVehicle vehicle = VechicleFactory.GetVehicle(numberofTires);
+                    vehicle.Drive();                
+                }
+                else
+                {
+                    Console.Write($"{numberofTires} is not a valid input!! -- Enter another value : ");
+                    numberofTires = Console.ReadLine();
+                }
+            }  
         }
     }
 }
